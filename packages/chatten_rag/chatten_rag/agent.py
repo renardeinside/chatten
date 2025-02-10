7b12819a70d47191fb33963dc596ca7312f99932
renardeinside <polarpersonal@gmail.com>
2025-02-10 15:45:01 +0100

    fix runnable logic

--- packages/chatten_rag/chatten_rag/agent.py
+++ packages/chatten_rag/chatten_rag/agent.py
@@ -3,7 +3,7 @@ from typing import Iterator
 from databricks_langchain import ChatDatabricks
 from mlflow.langchain.output_parsers import ChatCompletionOutputParser
 from langgraph.prebuilt import create_react_agent
-from langchain_core.runnables import RunnableGenerator
+from langchain_core.runnables import RunnableLambda
 from langgraph.pregel.io import AddableValuesDict
 from langchain_core.documents import Document
 from langchain_core.messages import BaseMessage
@@ -19,12 +19,13 @@ class SerializedVectorSearchRetrieverTool(VectorSearchRetrieverTool):
         return json.dumps(_serialized)
 
 
-def get_agent(chat_model: str, vsi: str, prompt: str) -> RunnableGenerator:
+def get_agent(chat_model: str, vsi: str, prompt: str):
     retriever_tool = SerializedVectorSearchRetrieverTool(
         index_name=vsi,
         num_results=3,
         tool_name="retriever",
         tool_description="Search through document corpus stored in Vector Search Index. Provides helpful insights about Databricks.",
+        columns=["path"],
     )
 
     llm = ChatDatabricks(model=chat_model)
@@ -35,12 +36,10 @@ def get_agent(chat_model: str, vsi: str, prompt: str) -> RunnableGenerator:
         prompt=prompt,
     )
 
-    def wrap_output(stream: Iterator[AddableValuesDict]) -> Iterator[str]:
+    def wrap_output(state: AddableValuesDict) -> str:
+        messages: list[BaseMessage] = state["messages"]
+        packed_messages = [message.model_dump() for message in messages]
+        return json.dumps(packed_messages)
 
-        for event in stream:
-            messages: list[BaseMessage] = event["messages"]
-            packed_messages = [message.model_dump() for message in messages]
-            yield json.dumps(packed_messages)
-
-    agent = raw_agent | RunnableGenerator(wrap_output) | ChatCompletionOutputParser()
+    agent = raw_agent | RunnableLambda(wrap_output) | ChatCompletionOutputParser()
     return agent
